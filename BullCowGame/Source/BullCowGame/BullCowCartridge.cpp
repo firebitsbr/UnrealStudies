4b9f5f03866d5e5a0fe2765b7e03f67208248c76
Rafael <rafael.ocariz90@gmail.com>
2020-04-27 21:39:42 -0300

    Finished BullsCows Game

--- BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,5 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+#include "HiddenWordList.h"
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
@@ -9,36 +10,17 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     PrintLine(TEXT("Press Enter to continue..."));
 
     ClearHiddenWordAndLifes();
+    SetValidWords();
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
     ClearScreen();
-    
-    if(!HiddenWord.Equals(TEXT("")))
+
+    if (!HiddenWord.Equals(TEXT("")))
     {
-        if(Lifes == 0 && Replay(Input)) return;
-
-        if(!IsValidInput(Input))
-        {
-            PrintLine(TEXT("Invalid input, try again."));
-            PrintHint();
+        if (!ProcessInput(Input))
             return;
-        }
-
-        if(!Input.Equals(HiddenWord))
-        {
-            LostEvent(Input);
-
-            if(Lifes > 0)
-            {
-                PrintHint();
-                PrintLine(TEXT("You have %i Bulls and %i Cows"), Bulls, Cows);
-            }
-            
-            return;
-        }
-
         WinEvent();
     }
 
@@ -47,14 +29,27 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
 }
 
 void UBullCowCartridge::ClearHiddenWordAndLifes() noexcept
-{ 
-	HiddenWord = TEXT(""); 
-	Lifes = 0;
+{
+    HiddenWord = TEXT("");
+    Lifes = 0;
+}
+
+void UBullCowCartridge::SetValidWords() noexcept
+{
+    for (auto &Word : Words)
+    {
+        if (IsIsogram(Word) && Word.Len() < 10)
+        {
+            ValidWords.Emplace(Word);
+        }
+    }
 }
 
 void UBullCowCartridge::InitGame() noexcept
 {
-    HiddenWord = TEXT("cake");
+    int32 Index = FMath::RandRange(0, ValidWords.Num() - 1);
+    HiddenWord = ValidWords[Index];
+    ValidWords.RemoveAt(Index);
     Lifes = HiddenWord.Len();
 }
 
@@ -65,22 +60,43 @@ void UBullCowCartridge::PrintHint() noexcept
     PrintLine(TEXT("You have %i more %s"), Lifes, *temp);
 }
 
-bool UBullCowCartridge::IsValidInput(const FString& Input) noexcept
+bool UBullCowCartridge::ProcessInput(const FString &Input) noexcept
+{
+    if (Lifes == 0 && Replay(Input))
+        return false;
+
+    if (!IsValidInput(Input))
+    {
+        PrintLine(TEXT("Invalid input, try again."));
+        PrintHint();
+        return false;
+    }
+
+    if (!Input.Equals(HiddenWord))
+    {
+        LostEvent(Input);
+        return false;
+    }
+
+    return true;
+}
+
+bool UBullCowCartridge::IsValidInput(const FString &Input) noexcept
 {
     auto bHasSameLength = Input.Len() == HiddenWord.Len();
     return bHasSameLength && IsIsogram(Input);
 }
 
-bool UBullCowCartridge::IsIsogram(const FString& Input) const noexcept
+bool UBullCowCartridge::IsIsogram(const FString &Input) const noexcept
 {
     auto bReturnValue = false;
-
     TSet<TCHAR> Letters;
 
-    for(auto& Letter : Input)
+    for (auto &Letter : Input)
     {
         Letters.Add(Letter, &bReturnValue);
-        if(bReturnValue) break;
+        if (bReturnValue)
+            break;
     }
 
     return !bReturnValue;
@@ -92,46 +108,62 @@ void UBullCowCartridge::WinEvent() noexcept
     PrintLine(TEXT("Let's go to the next word!"));
     PrintLine(TEXT(""));
 }
-	
-void UBullCowCartridge::LostEvent(const FString& Input) noexcept
+
+void UBullCowCartridge::LostEvent(const FString &Input) noexcept
 {
     --Lifes;
 
-    if(Lifes > 0)
+    if (Lifes > 0)
     {
-        GetAmountOfBullsAndCows(Input);
+        auto Count = GetBullsCows(Input);
+
+        PrintHint();
+        PrintLine(TEXT("You have %i Bulls and %i Cows"), Count.Bulls, Count.Cows);
         return;
     }
 
     PrintLine(TEXT("You're lost all your chances!"));
     PrintLine(TEXT("Maybe next time."));
-    
+
     PrintLine(TEXT(""));
     PrintLine(TEXT("Do you want to restart the game? (Y/N)"));
 }
 
-void UBullCowCartridge::GetAmountOfBullsAndCows(const FString& Input) noexcept
+FBullsCowsCount UBullCowCartridge::GetBullsCows(const FString &Input) const noexcept
 {
-    Bulls = 0;
-    Cows = 0;
+    FBullsCowsCount Count{0, 0};
 
-    for(int32 i = 0; i < HiddenWord.Len() && i < Input.Len(); ++i)
+    for (int32 Index = 0; Index < HiddenWord.Len(); ++Index)
     {
-        if(HiddenWord[i] == Input[i]) ++Bulls;
-        else ++Cows;
+        if (HiddenWord[Index] == Input[Index])
+        {
+            ++Count.Bulls;
+            continue;
+        }
+
+        for (auto &Letter : HiddenWord)
+        {
+            if (Input[Index] == Letter)
+            {
+                ++Count.Cows;
+                break;
+            }
+        }
     }
+
+    return Count;
 }
 
-bool UBullCowCartridge::Replay(const FString& Input) noexcept
+bool UBullCowCartridge::Replay(const FString &Input) noexcept
 {
     auto bReturnValue = false;
 
-    if(Input.Equals("Y"))
+    if (Input.Equals("Y"))
     {
         ClearHiddenWordAndLifes();
         bReturnValue = true;
     }
-    else if(Input.Equals("N"))
+    else if (Input.Equals("N"))
     {
         // Close the game
     }
@@ -139,6 +171,6 @@ bool UBullCowCartridge::Replay(const FString& Input) noexcept
     {
         PrintLine(TEXT("Incorrect Input! Try again."));
     }
-    
+
     return bReturnValue;
 }
--- BullCowGame/Source/BullCowGame/BullCowCartridge.h
+++ BullCowGame/Source/BullCowGame/BullCowCartridge.h
@@ -1,40 +1,47 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Console/Cartridge.h"
 #include "BullCowCartridge.generated.h"
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+struct FBullsCowsCount
+{
+	int32 Bulls;
+	int32 Cows;
+};
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 {
 	GENERATED_BODY()
 
-	public:
+public:
 	virtual void BeginPlay() override;
-	virtual void OnInput(const FString& Input) override;
+	virtual void OnInput(const FString &Input) override;
 
 	// Your declarations go below!
-	private:
-	TArray<FString> ListOfWords;
+private:
+	TArray<FString> ValidWords;
 	FString HiddenWord;
 
 	int32 Lifes;
-	int32 Bulls;
-	int32 Cows;
 
 	void ClearHiddenWordAndLifes() noexcept;
+
+	void SetValidWords() noexcept;
 
 	void InitGame() noexcept;
 	void PrintHint() noexcept;
 
-	bool IsValidInput(const FString& Input) noexcept;
-	bool IsIsogram(const FString& Input) const noexcept;
+	bool ProcessInput(const FString&) noexcept;
+	bool IsValidInput(const FString&) noexcept;
+	bool IsIsogram(const FString&) const noexcept;
 
 	void WinEvent() noexcept;
-	void LostEvent(const FString& Input) noexcept;
-	void GetAmountOfBullsAndCows(const FString& Input) noexcept;
+	void LostEvent(const FString&) noexcept;
 
-	bool Replay(const FString& Input) noexcept;
+	FBullsCowsCount GetBullsCows(const FString&) const noexcept;
+
+	bool Replay(const FString&) noexcept;
 };
