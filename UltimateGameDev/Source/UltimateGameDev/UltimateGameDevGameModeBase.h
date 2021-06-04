6972bb114bf159dcab179254be862941338c6d93
rocariz <rocariz@demonware.net>
2021-06-04 12:15:10 +0100

    Added The Grid and UltimateGamedevCpp projects

---
+++ UltimateGameDev/Source/UltimateGameDev/UltimateGameDevGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UltimateGameDevGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ULTIMATEGAMEDEV_API AUltimateGameDevGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
